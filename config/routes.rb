42901c201884d16071914b43fc12a7eac9ed2c85
hectortllo <hectortllo@gmail.com>
2020-03-21 15:54:43 -0600

    Se creó un health endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Haciendo un get a la ruta /health
+    #Vaya al controlador healt y al método health
+  get '/health', to: 'health#health'
 end
