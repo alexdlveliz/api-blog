0870ea44dde3f23e9ced52ebbe837ff03b5c77d1
hectortllo <hectortllo@gmail.com>
2020-03-21 21:34:11 -0600

    1. Se agregó la gema active_model_serializers. 2. Se creó el serializer para el modelo post

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,15 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :id, :title, :content, :published, :author
+
+  #En los atributos definimos un 'author', pero debido a que
+  #en el modelo 'author' no existe, para eso declaramos el 
+  #método author: 
+  def author
+    user = self.object.user
+    {
+      name: user.name,
+      email: user.email,
+      id: user.id
+    }
+  end
+end
