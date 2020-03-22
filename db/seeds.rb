94408eb465e6b14df92c3fc56359ed9c94e0363e
hectortllo <hectortllo@gmail.com>
2020-03-21 22:29:06 -0600

    Se agregó contenido al archivo seeds.rb para probar el modelo post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+5.times do |user|
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    auth_token: "xxx"
+  )
+end
+
+10.times do |post|
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph,
+    published: true,
+    user_id: User.first.id
+  )
+end
