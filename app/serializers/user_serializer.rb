6a141610ce83e04374c8848f9a1d764105037879
hectortllo <hectortllo@gmail.com>
2020-04-21 01:42:12 -0600

    Se devolvió el id del usuario al momento de hacer el login

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@
 # asociamos comentarios y posts para utilizarlos
 # posteriormente si es necesario
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :username
+  attributes :name, :username, :id
   has_many :posts
   has_many :comments
 end
