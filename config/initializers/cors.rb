98a166885b7b05855cf35fb330d18aae4fbd170e
hectortllo <hectortllo@gmail.com>
2020-04-23 15:25:29 -0600

    Se permite el acceso a netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://cranky-benz-e49305.netlify.app'
+    origins '*'
 
     resource '*',
       headers: :any,
